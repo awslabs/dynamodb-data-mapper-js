7c04c83607b1a33df8fab438f1cc2a15aa74148c
Jonathan Eskew <eskewj@amazon.com>
2018-06-16 11:13:59 -0700

    Serialize all key properties provided in startKey and no others

---
+++ packages/dynamodb-data-mapper/src/marshallStartKey.ts
@@ -0,0 +1,24 @@
+import { marshallValue, Schema } from '@aws/dynamodb-data-marshaller';
+import { Key } from 'aws-sdk/clients/dynamodb';
+
+/**
+ * @internal
+ */
+export function marshallStartKey(
+    schema: Schema,
+    startKey: {[key: string]: any}
+): Key {
+    const key: Key = {};
+    for (const propertyName of Object.keys(startKey)) {
+        const propSchema = schema[propertyName];
+        const { attributeName = propertyName } = propSchema;
+        if (propSchema) {
+            key[attributeName] = marshallValue(
+                propSchema,
+                startKey[propertyName]
+            )!;
+        }
+    }
+
+    return key;
+}
