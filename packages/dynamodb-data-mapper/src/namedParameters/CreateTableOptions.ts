5c8bd8c337085a83d833dcffec9e484319f92ba2
Jonathan Eskew <jeskew@users.noreply.github.com>
2019-01-31 23:05:23 +0900

    Update packages/dynamodb-data-mapper/src/namedParameters/CreateTableOptions.ts
    Co-Authored-By: sh1n1chi8acker <shinichi.hatayama@jp.sony.com>

--- packages/dynamodb-data-mapper/src/namedParameters/CreateTableOptions.ts
+++ packages/dynamodb-data-mapper/src/namedParameters/CreateTableOptions.ts
@@ -4,7 +4,21 @@ import { PerIndexOptions } from './SecondaryIndexOptions';
 export interface CreateTableOptions extends ProvisionedThroughput {
     streamViewType?: StreamViewType;
     indexOptions?: PerIndexOptions;
+interface BaseCreateTableOptions {
+    streamViewType?: StreamViewType;
+    indexOptions?: PerIndexOptions;
     billingMode?: BillingMode;
+}
+
+export interface ProvisionedCreateTableOptions extends ProvisionedThroughput, BaseCreateTableOptions {
+    billingMode?: 'PROVISIONED';
+}
+
+export interface OnDemandCreateTableOptions extends BaseCreateTableOptions {
+    billingMode: 'PAY_PER_REQUEST';
+}
+
+export type CreateTableOptions = ProvisionedCreateTableOptions | OnDemandCreateTableOptions;
 }
 
 export type BillingMode = 'PROVISIONED' | 'PAY_PER_REQUEST';
