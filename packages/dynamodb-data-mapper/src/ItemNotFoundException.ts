3dc007c07f7808cee6d5a9588ee684ead98f5fd6
Jake Champion <jakechampion.jake2@googlemail.com>
2019-11-05 12:19:07 +0000

    Use Object.setPrototypeOf if it exists, fallback to modifying __proto__ property if it does not exist

--- packages/dynamodb-data-mapper/src/ItemNotFoundException.ts
+++ packages/dynamodb-data-mapper/src/ItemNotFoundException.ts
@@ -1,4 +1,10 @@
-import {GetItemInput} from "aws-sdk/clients/dynamodb";
+import { GetItemInput } from "aws-sdk/clients/dynamodb";
+const {
+    setPrototypeOf = function (obj: any, proto: any) {
+        obj.__proto__ = proto;
+        return obj;
+    },
+} = Object;
 
 /**
  * An exception thrown when an item was sought with a DynamoDB::GetItem
@@ -8,7 +14,6 @@ import {GetItemInput} from "aws-sdk/clients/dynamodb";
 export class ItemNotFoundException extends Error {
     readonly name = 'ItemNotFoundException';
 
-    __proto__: Error;
     constructor(
         public readonly itemSought: GetItemInput,
         message: string = defaultErrorMessage(itemSought)
@@ -16,12 +21,12 @@ export class ItemNotFoundException extends Error {
         super(message);
 
         // https://github.com/Microsoft/TypeScript/wiki/Breaking-Changes#extending-built-ins-like-error-array-and-map-may-no-longer-work
-        this.__proto__ = ItemNotFoundException.prototype;
+        setPrototypeOf(this, ItemNotFoundException.prototype);
     }
 }
 
 function defaultErrorMessage(itemSought: GetItemInput): string {
     return `No item with the key ${
         JSON.stringify(itemSought.Key)
-    } found in the ${itemSought.TableName} table.`;
+        } found in the ${itemSought.TableName} table.`;
 }
