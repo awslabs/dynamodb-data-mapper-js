4d8d8ee24c76efc80bea72a273b365ecb37ca720
Etienne Noel <etiennenoel@gmail.com>
2021-06-01 17:24:23 -0400

    Quick fix for something else.

--- packages/dynamodb-data-marshaller/src/marshallItem.ts
+++ packages/dynamodb-data-marshaller/src/marshallItem.ts
@@ -9,7 +9,41 @@ import {
     Marshaller,
     NumberValueSet,
 } from '@awslabs-community-fork/dynamodb-auto-marshaller';
-import {bytes} from "./utf8-bytes";
+
+const convertToBytes = (str): any[] => {
+    var bytes: any[] = [];
+    for (var i = 0; i < str.length; i++) {
+        var c = str.charCodeAt(i);
+        if (c >= 0xd800 && c <= 0xdbff && i + 1 < str.length) {
+            var cn = str.charCodeAt(i + 1);
+            if (cn >= 0xdc00 && cn <= 0xdfff) {
+                var pt = (c - 0xd800) * 0x400 + cn - 0xdc00 + 0x10000;
+
+                bytes.push(
+                    0xf0 + Math.floor(pt / 64 / 64 / 64),
+                    0x80 + Math.floor(pt / 64 / 64) % 64,
+                    0x80 + Math.floor(pt / 64) % 64,
+                    0x80 + pt % 64
+                );
+                i += 1;
+                continue;
+            }
+        }
+        if (c >= 2048) {
+            bytes.push(
+                0xe0 + Math.floor(c / 64 / 64),
+                0x80 + Math.floor(c / 64) % 64,
+                0x80 + c % 64
+            );
+        }
+        else if (c >= 128) {
+            bytes.push(0xc0 + Math.floor(c / 64), 0x80 + c % 64);
+        }
+        else bytes.push(c);
+    }
+    return bytes;
+};
+
 
 /**
  * Converts a JavaScript object into a DynamoDB Item.
@@ -273,7 +307,7 @@ function marshallBinary(
         return new Uint8Array(input);
     }
 
-    return Uint8Array.from(bytes(input));
+    return Uint8Array.from(convertToBytes(input));
 }
 
 function marshallNumber(input: number): string {
